60579ae52b25f0992adf90b22a4830220d405f3c
CanadianPotatoe <Kanhay.Patil@gmail.com>
2024-12-31 08:56:14 -0800

    All thats left is tuning PID

--- main.py
+++ main.py
@@ -68,17 +68,17 @@ pwm_a.init(freq=5000, duty_ns=5000)
 pwm_b.init(freq=5000, duty_ns=5000)
 
 # PID Parameters
-Kp_straight = 0.5
+Kp_straight = 0.05
 Ki_straight = 0.0
-Kd_straight = 0.0
-Kp_distance = 0.0
+Kd_straight = 5
+Kp_distance = 5
 Ki_distance = 0.0
 Kd_distance = 0.0
 Kp_turn = 0.01
-Ki_turn = 0
+Ki_turn = 0.00000
 Kd_turn = 0
 deadband_distance = 10
-deadband_turn = 0.07
+deadband_turn = 0.1
 
 # Push button on GPIO 22
 button = Pin(22, Pin.IN, Pin.PULL_UP)
@@ -87,8 +87,8 @@ def normalize_angle(angle):
     """Normalize an angle to the range [0, 360)."""
     return (angle+360) % 360
 
-min_speed=0.295
-max_turn_speed=0.4
+min_speed=0.28
+max_turn_speed=0.32
 # Helper functions to control motor speed and direction
 def set_motor_speed_a(speed):
     motor_speed_1 = int(abs(speed) * 65535)
@@ -111,41 +111,26 @@ def set_motor_speed_b(speed):
     elif speed < 0:
         IN3.value(0)
         IN4.value(1)
-    if speed==0:  # Brake mode
-        IN3.value(1)
-        IN4.value(1)
     pwm_b.duty_u16(motor_speed_2)
-
-def set_motor_speed_b_straight(speed):
-    motor_speed_2 = int(abs(speed) * 2337)
-    if speed > 0:
-        IN3.value(1)
-        IN4.value(0)
-    elif speed < 0:
-        IN3.value(0)
-        IN4.value(1)
-    if speed==0:  # Brake mode
-        IN3.value(1)
-        IN4.value(1)
-    pwm_b.duty_u16(motor_speed_2)
-
 
 def calculate_motor_speed(turn_num, straight_num, target_time):
     global motor_speed
-    turn_time = 4  # Time for a 90-degree turn (in seconds)
+    turn_time = 3.5  # Time for a 90-degree turn (in seconds)
     total_turn_time = turn_time * turn_num
     remaining_time = target_time - total_turn_time
-    straight_time = 1.53  # Modify this based on your specific requirements
+    straight_time = 0.8  # time for one straight at 50% speed (in seconds)
     time_per_straight = remaining_time / straight_num
-    motor_speed = 50 * (straight_time / time_per_straight)
+    motor_speed = 0.50 * (straight_time / time_per_straight)
 
     # Ensure motor speed does not exceed 100%
-    if motor_speed > 100:
-        motor_speed = 100
+    if motor_speed > 1:
+        motor_speed = 1
         
     return motor_speed
 
 def turn_left():
+    set_motor_speed_a(0)
+    set_motor_speed_b(0)
     global target_yaw
     target_yaw = normalize_angle(initial_yaw + 90)
 
@@ -162,12 +147,11 @@ def turn_left():
         P_turn = turn_error * Kp_turn
         I_turn = error_sum_turn * Ki_turn
         D_turn = (turn_error - last_error_turn) * Kd_turn
-        correction_turn = P_turn
-        
+        correction_turn = P_turn + I_turn 
+    
     # Calculate motor speeds based on correction
         speed_a = correction_turn
         speed_b = -correction_turn
-
         # Ensure motors stop at the correct target
         if abs(turn_error) < deadband_turn:
             speed_a = 0
@@ -254,7 +238,8 @@ def turn_right():
     return target_yaw
 
 def forward(segments):
-
+    yaw = normalize_angle(bno.euler[2])
+    straight_error = target_yaw - yaw
     error_sum_straight = 0
     last_error_straight = 0
     encoder_a = Encoder(14, 15)
@@ -263,13 +248,14 @@ def forward(segments):
     wheel_diameter=6
     encoder_resolution=1440
     wheel_circumference=math.pi*wheel_diameter
-    encoder_distance=(total_distance/wheel_circumference)*encoder_resolution
+    encoder_distance=(total_distance/wheel_circumference)*encoder_resolution/abs((math.cos(straight_error)))
     traveled_distance=(encoder_a.position()+(-1*encoder_b.position()))/2
-
-    while traveled_distance<encoder_distance:
+    distance_error=encoder_distance-traveled_distance
+    while abs(distance_error)>deadband_distance:
         yaw = normalize_angle(bno.euler[2])
         straight_error = target_yaw - yaw
         traveled_distance=(encoder_a.position()+(-1*encoder_b.position()))/2
+        distance_error=encoder_distance-traveled_distance
         # Normalize the turn error
         if straight_error > 180:
             straight_error -= 360
@@ -281,10 +267,15 @@ def forward(segments):
         I_straight = error_sum_straight * Ki_straight
         D_straight = (straight_error - last_error_straight) * Kd_straight
         correction_straight = P_straight + I_straight + D_straight
-        
+        if abs(distance_error)<1440:
+        #Distance PID Calculations:
+            P_distance = distance_error * Kp_distance
+        else:
+            P_distance=0
+        motor_b_speed=-2.13414*motor_speed**3+3.74527*motor_speed**2-1.21031*motor_speed+0.429783
         # Calculate motor speeds based on correction
-        speed_a = motor_speed + correction_straight
-        speed_b = motor_speed - correction_straight
+        speed_a = motor_speed + correction_straight + P_distance
+        speed_b = motor_b_speed - correction_straight + P_distance
 
         # Apply minimum limits to avoid stalling
         if abs(speed_a) > 0 and abs(speed_a) < min_speed:
@@ -298,35 +289,38 @@ def forward(segments):
 
         # Send speeds to motors
         set_motor_speed_a(speed_a)
-        set_motor_speed_b_straight(speed_b)
+        set_motor_speed_b(1*speed_b)
 
         error_sum_straight += straight_error
         last_error_straight = straight_error
         
         print(f"yaw: {yaw}, distance traveled: {traveled_distance}, target distance: {encoder_distance}, Speed A: {speed_a}, Speed B: {speed_b}")
-    set_motor_speed_a(0.05)
-    set_motor_speed_b(0.05)
+    
+    set_motor_speed_a(-0.05)
+    set_motor_speed_b(-0.05)
     global initial_yaw
     initial_yaw=target_yaw
     time.sleep(1)
 
 def backward(segments):
-
+    yaw = normalize_angle(bno.euler[2])
+    straight_error = target_yaw - yaw
     error_sum_straight = 0
     last_error_straight = 0
     encoder_a = Encoder(14, 15)
-    encoder_b= Encoder(11,10)
-    total_distance=segments*25
-    wheel_diameter=6
-    encoder_resolution=1440
-    wheel_circumference=math.pi*wheel_diameter
-    encoder_distance=(total_distance/wheel_circumference)*encoder_resolution
-    traveled_distance=(encoder_b.position()+(-1*encoder_a.position()))/2
-
-    while traveled_distance<encoder_distance:
+    encoder_b = Encoder(11, 10)
+    total_distance = segments * 25
+    wheel_diameter = 6
+    encoder_resolution = 1440
+    wheel_circumference = math.pi * wheel_diameter
+    encoder_distance = (total_distance / wheel_circumference) * encoder_resolution / abs((math.cos(straight_error)))
+    traveled_distance = (encoder_a.position() + (-1 * encoder_b.position())) / -2
+    distance_error = encoder_distance - traveled_distance
+    while abs(distance_error) > deadband_distance:
         yaw = normalize_angle(bno.euler[2])
         straight_error = target_yaw - yaw
-        traveled_distance=(encoder_b.position()+(-1*encoder_a.position()))/2
+        traveled_distance = (encoder_a.position() + (-1 * encoder_b.position())) / -2
+        distance_error = encoder_distance - traveled_distance
         # Normalize the turn error
         if straight_error > 180:
             straight_error -= 360
@@ -338,10 +332,16 @@ def backward(segments):
         I_straight = error_sum_straight * Ki_straight
         D_straight = (straight_error - last_error_straight) * Kd_straight
         correction_straight = P_straight + I_straight + D_straight
-        
+        if abs(distance_error) < 1440:
+            # Distance PID Calculations:
+            P_distance = -distance_error * Kp_distance
+        else:
+            P_distance = 0
+
+        motor_b_speed = -2.13414 * motor_speed**3 + 3.74527 * motor_speed**2 - 1.21031 * motor_speed + 0.429783
         # Calculate motor speeds based on correction
-        speed_a = motor_speed - correction_straight
-        speed_b = motor_speed + correction_straight
+        speed_a = -motor_speed + correction_straight + P_distance
+        speed_b = -motor_b_speed - correction_straight + P_distance
 
         # Apply minimum limits to avoid stalling
         if abs(speed_a) > 0 and abs(speed_a) < min_speed:
@@ -354,18 +354,20 @@ def backward(segments):
         speed_b = max(min(speed_b, motor_speed), -motor_speed)
 
         # Send speeds to motors
-        set_motor_speed_a(-speed_a)
-        set_motor_speed_b_straight(-speed_b)
+        set_motor_speed_a(speed_a)
+        set_motor_speed_b(1 * speed_b)
 
         error_sum_straight += straight_error
         last_error_straight = straight_error
-        
+
         print(f"yaw: {yaw}, distance traveled: {traveled_distance}, target distance: {encoder_distance}, Speed A: {speed_a}, Speed B: {speed_b}")
+    
     set_motor_speed_a(0.05)
     set_motor_speed_b(0.05)
     global initial_yaw
-    initial_yaw=target_yaw
+    initial_yaw = target_yaw
     time.sleep(1)
+
 
 # Main loop to check for button press and execute commands
 while True:
@@ -387,12 +389,12 @@ while True:
         time.sleep(0.2)
         set_motor_speed_a(0.05)
         set_motor_speed_b(0.05)
-        target_time = 85
+        target_time = 55
         turn_num = 8
-        straight_num = 70
+        straight_num = 50
         target_yaw = normalize_angle(bno.euler[2])
         motor_speed = calculate_motor_speed(turn_num, straight_num, target_time)
-        forward(1.33)
+        forward(1.3)
         turn_left()
         forward(3)
         turn_right()
